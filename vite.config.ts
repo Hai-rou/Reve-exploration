55f264fcf2b21a36e935f828818415b1beca2e10
Houmadi Hairou <hairou.houmadi@gmail.com>
2025-10-31 19:31:02 +0100

    save vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome80',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
