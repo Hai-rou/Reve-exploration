557bd5a1b985d5461eb9ec0205d8a38a2ae85b69
Houmadi Hairou <hairou.houmadi@gmail.com>
2025-11-20 13:14:27 +0100

    save admin mode

--- api/[...slug].cjs
+++ api/[...slug].cjs
@@ -1,3 +1,8 @@
+// Catch-all Vercel serverless function for /api/* using CommonJS
+const serverless = require('serverless-http');
+const app = require('../server/app');
+
+module.exports = serverless(app);
 // Catch-all API serverless function for Vercel (CommonJS to match existing Express code)
 const serverless = require('serverless-http');
 const app = require('../server/app');
